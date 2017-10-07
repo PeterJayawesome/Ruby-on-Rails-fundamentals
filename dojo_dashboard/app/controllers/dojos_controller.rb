fe068e0c994675a6ced0c5297c5f72cb37d4c0dd
Peng Jiang <pengj@usc.edu>
2017-10-07 10:55:41 -0700

    finish dojo_dashboard

--- dojo_dashboard/app/controllers/dojos_controller.rb
+++ dojo_dashboard/app/controllers/dojos_controller.rb
@@ -3,4 +3,39 @@ class DojosController < ApplicationController
 		@dojos = Dojo.all
 		@count = Dojo.all.size
 	end
+
+	def show
+		@dojo = Dojo.find(params[:id])
+	end
+
+	def edit
+		@dojo = Dojo.find(params[:id])
+	end
+
+	def destroy
+		dojo = Dojo.find(params[:id])
+		dojo.destroy
+		redirect_to "/dojos"
+	end
+
+	def new
+
+	end
+
+	def create
+		dojo = Dojo.create(form_params)
+		redirect_to "/dojos"
+	end
+
+	def update
+		dojo = Dojo.find_by(params[:id])
+		dojo.update(form_params)
+		redirect_to "/dojos/#{dojo.id}"
+	end
+
+	private
+	def form_params
+		params.require(:dojo).permit(:branch,:street,:city,:state)
+		
+	end
 end
--- dojo_dashboard/config/routes.rb
+++ dojo_dashboard/config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get 'dojos' => "dojos#index"
+  get 'dojos/new' => "dojos#new"
+  get 'dojos/:id' => "dojos#show"
+  get 'dojos/:id/edit' => "dojos#edit"
+  delete 'dojos/:id' => "dojos#destroy"
+  patch 'dojos/:id' => "dojos#update"
+  post 'dojos' => "dojos#create"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
